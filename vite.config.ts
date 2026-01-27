d281bc21d2768e1a62184ea3af18a00850e37e39
Mehul Singh Teya <mehulsaini456@gmail.com>
2026-01-28 01:24:13 +0530

    changed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/web-sudoku/',
+  base: '/zudoku/',
   build: {
     outDir: 'docs',
   },
